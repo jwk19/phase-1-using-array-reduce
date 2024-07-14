a721ce6eed549ac993d76af446b2d65f5f0a434e
James <spesalink@gmail.com>
2024-07-15 00:22:35 +0300

    Used the reduce metthod to calculate total sum of batteries.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
 // Code your solution here
+
+// create variable batteryBatches
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+//create variable totalBatteries and using the reduce amount to calculate the total sum on all batteries
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+
+console.log(totalBatteries); // result will be 31
